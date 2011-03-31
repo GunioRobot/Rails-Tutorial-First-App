f868958e4608f71898a98ac00d84becb47e0bc55
Allison Urban <allison@mailchimp.com>
2011-03-31 13:20:27 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'd9b7c699cb9962ca14c7a21abde7e48c165322e4266bb6d37581c936718a83d3bef3a30dad353b1843a19f51a8a3636628eba2c72df580e543f8abddf2942409'
